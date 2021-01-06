ccfa343f89a5d00bc810df7e61d49f2611753ca5
rhdong <rhdong2017@gmail.com>
2021-01-06 20:36:26 +0800

    a fake PR

--- tensorflow_recommenders_addons/dynamic_embedding/python/ops/dynamic_embedding_ops.py
+++ tensorflow_recommenders_addons/dynamic_embedding/python/ops/dynamic_embedding_ops.py
@@ -43,7 +43,7 @@ from tensorflow.python.training.tracking import base as trackable
 from tensorflow.python.util import compat
 from tensorflow.python.util.tf_export import tf_export
 
-
+ 
 class TrainableWrapper(resource_variable_ops.ResourceVariable):
   """
   This class is a trainable wrapper of Dynamic Embedding,
